6e2225a73cceaffcd107edd7842d032c93c6839b
DISEÑADORA <juanrobledo26@gmail.com>
2026-01-12 19:18:29 +0100

    modifica se ha imprimido por pantalla tabla del 12 por Juan Robledo

--- TablaDel12.java
+++ TablaDel12.java
@@ -5,7 +5,9 @@ public class TablaDel12
         for(int i = 0; i <= 10; i++)
         {
             System.out.println("12 * "+ i + " = " + 12 * i + "\n");
-> Esta es la tabla del 12
+> Se ha imprimido por pantalla la tabla del 12.
+
+
 
         }
     }
